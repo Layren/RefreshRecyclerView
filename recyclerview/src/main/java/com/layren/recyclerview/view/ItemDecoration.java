16d227c4bd0cac3f78caedffb62da8e96e852c6c
Layren <2506196833@qq.com>
2020-07-14 15:05:49 +0800

    修复Bug. 添加 android:overScrollMode="never"

--- recyclerview/src/main/java/com/layren/recyclerview/view/ItemDecoration.java
+++ recyclerview/src/main/java/com/layren/recyclerview/view/ItemDecoration.java
@@ -3,8 +3,12 @@ package com.layren.recyclerview.view;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import androidx.annotation.ColorInt;
 import androidx.recyclerview.widget.GridLayoutManager;
@@ -18,10 +22,18 @@ import androidx.recyclerview.widget.RecyclerView;
  */
 
 public class ItemDecoration extends RecyclerView.ItemDecoration {
+    private static final String TAG = "ItemDecoration";
+
     private int dividerHeight;
     private int orientation;
     private Paint dividerPaint;
     private boolean isFrame = true;
+    private int countSize = 0;
+    private boolean isFirstLine = true;
+    private boolean isFirstColumn = true;
+    private boolean isLastLine = false;
+    private boolean isLastColumn = false;
+    private Map<Integer, ItemOffsets> offsetsMap = new HashMap<>();
 
     public ItemDecoration(int height, int orientation, @ColorInt int color) {
         dividerHeight = height;
@@ -75,24 +87,38 @@ public class ItemDecoration extends RecyclerView.ItemDecoration {
         } else {
             final int position = parent.getChildAdapterPosition(view);
             final int count = parent.getAdapter().getItemCount();
+            int spanSize = layoutManager.getSpanSizeLookup().getSpanSize(position);
+            int nextSpanSize = 0;
+            if (position < count - 1) {
+                nextSpanSize = layoutManager.getSpanSizeLookup().getSpanSize(position + 1);
+            }
+            if (count == 1) {
+                isLastLine = true;
+                isLastColumn = true;
+            }
+            if (position == 0) {
+                countSize = spanSize;
+            }
+
+            offsetsMap.put(position, new ItemOffsets(isFirstLine, isFirstColumn, isLastLine, isLastColumn));
             if (isFrame) {
-                if ((position % spanCount) == 0) {
+                if (isFirstColumn) {
                     //最左边
                     outRect.left = dividerHeight;
                 }
-                if (position < spanCount) {
+                if (isFirstLine) {
                     // 第一行
                     outRect.top = dividerHeight;
                 }
                 outRect.right = dividerHeight;
                 outRect.bottom = dividerHeight;
             } else {
-                if ((position == count - 1) && (position % spanCount) == (spanCount - 1)) {
+                if (isLastLine && isLastColumn) {
                     // 最后一个，如果也是最右边，那么就不需要偏移
-                } else if (position >= (count - (count % spanCount))) {
+                } else if (isLastLine) {
                     // 最下面一行，只要右边偏移就行
                     outRect.right = dividerHeight;
-                } else if ((position % spanCount) == (spanCount - 1)) {
+                } else if (isLastColumn) {
                     // 最右边一列，只要下面偏移就行
                     outRect.bottom = dividerHeight;
                 } else {
@@ -100,8 +126,30 @@ public class ItemDecoration extends RecyclerView.ItemDecoration {
                     outRect.set(0, 0, dividerHeight, dividerHeight);
                 }
             }
+            countSize += nextSpanSize;
+            if (countSize > spanCount) {
+                countSize = nextSpanSize;
+                isFirstColumn = true;
+                isFirstLine = false;
+            } else {
+                isFirstColumn = false;
+            }
+            if (countSize == spanCount) {
+                isLastColumn = true;
+            } else {
+                isLastColumn = false;
+            }
+
+            if (spanCount >= count - position - 1 && isFirstColumn) {
+                int surplusSize = 0;
+                for (int i = position + 1; i < count; i++) {
+                    surplusSize += layoutManager.getSpanSizeLookup().getSpanSize(i);
+                }
+                isLastLine = surplusSize <= spanCount;
+            }
         }
     }
+
 
     @Override
     public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
@@ -121,18 +169,21 @@ public class ItemDecoration extends RecyclerView.ItemDecoration {
             }
         } else {
             for (int i = 0; i < childCount; i++) {
+                ItemOffsets itemOffsets = offsetsMap.get(i);
+                if (itemOffsets == null) {
+                    return;
+                }
                 View view = parent.getChildAt(i);
                 float top = view.getTop();
                 float bottom = view.getBottom();
                 float left = view.getLeft();
                 float right = view.getRight();
-                int laseCount = childCount % spanCount;
                 if (isFrame) {
-                    if (i < spanCount) {
+                    if (itemOffsets.isFirstLine) {
                         //第一行
                         c.drawRect(left, top - dividerHeight, right, top, dividerPaint);
                     }
-                    if (i % spanCount == 0) {
+                    if (itemOffsets.isFirstColumn) {
                         //第一列
                         c.drawRect(left - dividerHeight, top, left, bottom, dividerPaint);
                         //左上角
@@ -145,20 +196,39 @@ public class ItemDecoration extends RecyclerView.ItemDecoration {
                     //右下角
                     c.drawRect(right, bottom, right + dividerHeight, bottom + dividerHeight, dividerPaint);
                 } else {
-                    if (childCount - i > (laseCount == 0 ? spanCount : laseCount)) {
+                    if (!itemOffsets.isLastLine) {
                         //下边线
                         c.drawRect(left, bottom, right, bottom + dividerHeight, dividerPaint);
                     }
-                    if ((i + 1) % spanCount != 0) {
+                    if (!itemOffsets.isLastColumn) {
                         //右边线
                         c.drawRect(right, top, right + dividerHeight, bottom, dividerPaint);
-                        //右下角
-                        c.drawRect(right, bottom, right + dividerHeight, bottom + dividerHeight, dividerPaint);
+                        if (!itemOffsets.isLastLine) {
+                            //右下角
+                            c.drawRect(right, bottom, right + dividerHeight, bottom + dividerHeight, dividerPaint);
+                        }
                     }
                 }
             }
         }
 
+    }
+
+    /**
+     * 保存Item的位置信息
+     */
+    private static class ItemOffsets {
+        boolean isFirstLine;
+        boolean isFirstColumn;
+        boolean isLastLine;
+        boolean isLastColumn;
+
+        public ItemOffsets(boolean isFirstLine, boolean isFirstColumn, boolean isLastLine, boolean isLastColumn) {
+            this.isFirstLine = isFirstLine;
+            this.isFirstColumn = isFirstColumn;
+            this.isLastLine = isLastLine;
+            this.isLastColumn = isLastColumn;
+        }
     }
 
 
